5b767858c161922b71a2b87510a32041cef03410
Camila <araldi.cami@gmail.com>
2021-06-15 21:26:25 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news | home</title>
+      </Head>
+      <h1>Hello</h1>
+    </>
+  );
+}
