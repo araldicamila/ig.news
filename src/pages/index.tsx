556cec3eee191506051ae060111a1264de9009b4
Camila Araldi <araldi.cami@gmail.com>
2021-06-28 19:46:48 -0300

    Página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>ig.news | home</title>
       </Head>
-      <h1>Hello</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>Get acces to all publications <br ></br>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
