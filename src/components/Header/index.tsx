c567f0610d1092c7f7342c4a91ba250cfb6b4e46
Camila <araldi.cami@gmail.com>
2021-06-28 19:01:18 -0300

    Iniciando o header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header>
+      <div>
+        <img src="/images/logo.svg" alt="Logo" />
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
