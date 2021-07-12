414804f7033d7b33661a35c74c6a8cc119ae327e
Camila <araldi.cami@gmail.com>
2021-07-12 19:04:44 -0300

    Utilizando FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { fauna } from './../../../services/fauna';
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { query } from 'faunadb';
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,39 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      console.log(user)
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection("users"),
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 });
